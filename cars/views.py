d9d5bcf8c2676ceda19f734e7035f3ddf9982389
Railan Deivid <contato.railandeivid@gmail.com>
2025-04-07 20:18:07 -0300

    Update: Create views 'cars' and templates

--- cars/views.py
+++ cars/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from cars.models import Car
 
-# Create your views here.
+
+def cars_viws(request):
+    cars = Car.objects.all()
+    
+    return render(
+                request, 
+                "cars.html", 
+                {'Cars': cars}
+    )
