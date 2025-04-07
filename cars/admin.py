e28d314bf6b864e68eb530bd2e2c22703164333d
Railan Deivid <contato.railandeivid@gmail.com>
2025-04-07 18:04:19 -0300

    Update: Create model data table 'brand' and addd data

--- cars/admin.py
+++ cars/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
-from cars.models import Car
+from cars.models import Car, Brand
 
+
+
+class BrandAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+    search_fields = ('name',)
 
 class CarAdmin(admin.ModelAdmin):
     list_display = ('model', 'brand', 'factory_year', 'model_year', 'value')
@@ -8,4 +13,6 @@ class CarAdmin(admin.ModelAdmin):
     list_filter = ('factory_year', 'model_year')
 
 
+
+admin.site.register(Brand, BrandAdmin)
 admin.site.register(Car, CarAdmin)
