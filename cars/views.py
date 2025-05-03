1e00767982a7a81a1849a317ca9b0024a32fd329
Railan Deivid <contato.railandeivid@gmail.com>
2025-05-03 13:28:09 -0300

    Update: CSS page car adjustments

--- cars/views.py
+++ cars/views.py
@@ -1,17 +1,25 @@
 from django.shortcuts import render
 from cars.models import Car
-
+from django.core.paginator import Paginator
 
 def cars_viws(request):
-    
     cars = Car.objects.all().order_by('model')
-    search =  request.GET.get('search')
-    
+    search = request.GET.get('search')
+
     if search:
         cars = cars.filter(model__icontains=search)
-    
-    return render(
-                request, 
-                "cars.html", 
-                {'cars': cars}
-    )
+
+    # Paginação - 50 carros por página
+    paginator = Paginator(cars, 50)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    context = {
+        'cars': page_obj,  
+        'total_results': paginator.count,
+        'start_index': page_obj.start_index(),
+        'end_index': page_obj.end_index(),
+        'search': search  
+    }
+
+    return render(request, "cars.html", context)
